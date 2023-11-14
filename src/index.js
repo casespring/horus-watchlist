84532b5b1ba876613d10620963c63a5c543523f5
Marco Upia <markremembers@gmail.com>
2023-11-14 10:04:02 -0500

    added a selector and cleaned up some html and css

--- src/index.js
+++ src/index.js
@@ -1,37 +1,38 @@
-
-function handleSearch(){ 
-    let searchMedium = 'movie'
-
-let form = document.querySelector('#search-button')
-
-form.addEventListener('submit', (e)=>{
-    e.preventDefault()
-    fetch (`https://api.simkl.com/search/${searchMedium}?q=${searchParser(e)}&client_id=dd7675f0ec853dbe3f15e18e3bf9c23f45d586a0b6cce7369149e57836a633c0`)
-    .then((res)=>res.json())
-    .then((data)=>{
-        console.log(data)
-        data.forEach(handleImages);
+let form = document.querySelector('#search-button');
+let selector = document.querySelector("#category-options")
+let searchMedium = 'movie';
+selector.addEventListener("change", e => {
+    searchMedium = 'movie'
+    console.log(e.target.value);
+    let category = e.target.value;
+    console.log(category);
+    searchMedium = category;
+    
+    form.addEventListener('submit', (e)=>{
+        e.preventDefault()
+        fetch (`https://api.simkl.com/search/${searchMedium}?q=${searchParser(e)}&client_id=dd7675f0ec853dbe3f15e18e3bf9c23f45d586a0b6cce7369149e57836a633c0`)
+        .then((res)=>res.json())
+        .then((data)=>{
+            console.log(data);
+            data.forEach(handleImages);
+        })
+        e.target["search-button"].value = ""
     })
-    
 })
-
-}
-
-
-function searchParser(e){
-    let search = e.target['search-button'].value
-    let searchHandler = [...search.split(' ')]
-    return searchHandler.join('+')
     
-}
-
-function handleImages(data){
-    let imageUrl = data.poster
-    let container = document.querySelector('#poster-container')
-    let moviePoster = document.createElement('img')
-
-    moviePoster.src = `https://wsrv.nl/?url=https://simkl.in/posters/${imageUrl}_c.jpg`
-    moviePoster.className = 'posters'
+    function searchParser(e){
+        let search = e.target['search-button'].value;
+        let searchHandler = [...search.split(' ')];
+        return searchHandler.join('+');
+    }
+    
+    function handleImages(data){
+        let imageUrl = data.poster;
+        let container = document.querySelector('#poster-container');
+        let moviePoster = document.createElement('img');
+        moviePoster.className = 'posters';
+        moviePoster.src = `https://wsrv.nl/?url=https://simkl.in/posters/${imageUrl}_c.jpg`;
+        console.log(data)
 
     container.appendChild(moviePoster)
 
@@ -40,8 +41,7 @@ function handleImages(data){
         document.querySelector('#selectedMovie').src= e.target.src
         renderInfo(id)
     })
-
-    }
+}
 
 function renderInfo(id){
     let medium = 'movies'
@@ -52,7 +52,7 @@ function renderInfo(id){
         let trailer = document.querySelector('#trailer')
         let overview = document.querySelector('#overview')
         let rating = document.querySelector('#rating')
-
+        console.log(info.trailers)
         trailer.href = `https://www.youtube.com/watch?v=${info.trailers[0].youtube}`
         trailer.textContent = info.title
         overview.textContent = `Synopsis: ${info.overview}`
@@ -60,8 +60,3 @@ function renderInfo(id){
     })
 }
 
-
-
-
-
-handleSearch()
